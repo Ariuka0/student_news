a88e4fff2937b784219ccbb4f12a8e8b31c4e0ec
kknas <142768324+Ariuka0@users.noreply.github.com>
2026-04-20 22:21:41 +0800

    Update SUPABASE_ANON_KEY in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://dzznfsjuqgrrcomcilxb.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_KZwr1xuMqOc7kB_6xmquiA_GMiYzkL9"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6em5mc2p1cWdycmNvbWNpbHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2OTE2NjQsImV4cCI6MjA5MjI2NzY2NH0.XykIqFTCS_7EDT7YNMKD1Djg6lMHbKTGDEhidn_62ts"
 };
